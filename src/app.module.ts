5ba45e6bde7781e6ca42a4409e1f4d802c3f636a
Osama <malik.usama5810@gmail.com>
2024-06-02 21:00:12 +0500

    exceptions filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { throttleOptions } from './config/throttleOptions';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
   imports: [
@@ -18,23 +20,8 @@ import { APP_GUARD } from '@nestjs/core';
     ProductsModule,
     OrdersModule,
     AuthModule,
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000,
-        limit: 3,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
-        limit: 20,
-      },
-      {
-        name: 'long',
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
+    ThrottlerModule.forRoot(throttleOptions),
+    MyLoggerModule,
   ],
   controllers: [AppController],
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsOptions } from './config/corsOptions';
 import { VersioningType } from '@nestjs/common';
+import { AllExceptionsFilter } from './all-exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
-  })
+  });
   await app.listen(3000);
 }
 bootstrap();
